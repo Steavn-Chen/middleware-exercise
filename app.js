280d743d8ca2863d42f3212132139a10a4983763
Steavn Chen <tom9876555@yahoo.com.tw>
2022-03-30 02:52:46 +0800

    專案程式碼整理雙引號改為單引號，結尾分號去除

--- app.js
+++ app.js
@@ -4,36 +4,36 @@ const moment = require('moment')
 const app = express()
 const port = 3000
 
-app.use('/',  (req, res, next) => {
+app.use('/', (req, res, next) => {
   const requestTime = Date.now()
-  const taipeiTime = moment(Date.now()).format("YYYY-MM-DD HH:mm:ss");
+  const taipeiTime = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
   const { method, originalUrl } = req
   res.on('finish', () => {
-    const responseTime = Date.now() 
-    const result = responseTime - requestTime;
+    const responseTime = Date.now()
+    const result = responseTime - requestTime
     console.log(
       `${taipeiTime} | ${method} from ${originalUrl} | total time : ${result} ms`
-    );
+    )
   })
   next()
 })
 
-app.get("/", (req, res) => {
-  res.send("列出全部 Todo");
-});
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
 
-app.get("/new", (req, res) => {
-  res.send(`<form action='/' method=POST>新增 Todo 頁面<button type="submit"> Submit </button></form>`);
-});
+app.get('/new', (req, res) => {
+  res.send(`<form action='/' method=POST>新增 Todo 頁面<button type="submit"> Submit </button></form>`)
+})
 
-app.get("/:id", (req, res) => {
-  res.send("顯示一筆 Todo");
-});
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
 
-app.post("/", (req, res) => {
-  res.send("新增一筆  Todo");
-});
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+  console.log(`App running on port ${port}`)
+})
